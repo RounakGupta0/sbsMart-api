40dcad0072a3020232d4187da4e921ba789ee44b
RounakGupta0 <rounakgupta620@gmail.com>
2026-05-09 20:23:00 +0530

    completed product collection

---
+++ models/Products.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    name: {
+        type : String,
+        required : true,
+        maxlength : 50
+    },
+
+    price : {
+        type: Number,
+        required : true
+    },
+
+    brand : {
+        type : String,
+        required : true
+    },
+
+    description : {
+        type : String,
+        default : '',
+        maxlength : 200
+    },
+    
+    category : {
+        type : String,
+        enum : ['Fashion', 'Electronics' , 'Mobiles', 'Sports','others'],
+        required : true,
+    },
+
+    images : [
+        {
+            imageUrl :{type : String},
+            imageId : {type : String}
+        }
+    ],
+
+    likeCount : {
+        type : Number,
+        default : 0
+    },
+
+    likedBy : [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+    }],
+
+    stock : {
+        type : Number,
+        default : 0
+    }
+
+})
